62ca487188f0513cc1bd5a45324d59d7bf1bb507
annarischitelli <annarischitelli94@gmail.com>
2020-11-21 17:14:24 -0700

    parts 1,2,3 in data analysis

--- EmployeeSQL/SQLcode.sql
+++ EmployeeSQL/SQLcode.sql
@@ -56,3 +56,23 @@ SELECT * FROM dept_manager
 SELECT * FROM employees
 SELECT * FROM salaries
 SELECT * FROM titles
+
+------------------------------------------
+--- DATA ANALYSIS ---
+-- 1. List employee number, last name, first name, sex, and salary.
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON employees.emp_no=salaries.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE EXTRACT(year from hire_date) = 1986
+
+--3. List the manager of each department and include: department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments
+INNER JOIN dept_manager ON departments.dept_no=dept_manager.dept_no
+INNER JOIN employees ON dept_manager.emp_no=employees.emp_no
+
+--4. 
